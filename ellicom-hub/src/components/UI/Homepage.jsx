bcc9b684e1b71dbf4aa47f89488ee91ee7811d3c
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-06-05 06:50:35 +0000

    tailwind implemented v4 implemented and configured

---
+++ ellicom-hub/src/components/UI/Homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <div className='bg-amber-500 font-bold'>Homepage</div>
+  )
+}
+
+export default Homepage;
---
+++ ellicom-hub/src/components/UI/logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const logo = () => {
+  return (
+    <div><img src="/assets/images/ellicom-logo.svg" alt="ellicom_logo" /></div>
+  )
+}
+
+export default logo
