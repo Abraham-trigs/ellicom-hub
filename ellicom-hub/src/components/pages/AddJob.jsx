30e1d2a7f5fa613b51b5a50dce96c8a53597f975
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-06-11 13:14:29 +0000

    JobCard developed, Job detials is next to be implemented.

--- ellicom-hub/src/components/pages/AddJob.jsx
+++ ellicom-hub/src/components/pages/AddJob.jsx
@@ -7,46 +7,105 @@ import Head from '../UI/Head';
 const  CTAddJob= () => {
   return (
     <>
-      <div className='border-gold border-b-1 h-23 mb-5'>
+      <div className=' fixed border-gold border-b-1 h-23 mb-5'>
         <div className=" mt-10 flex flex-col justify-end">
 
-        <div className="w-full ">
-
-          {/* <div className='scale-50'>
-            <NewJobButton />
-          </div> */}
-
-          <div className=' flex flex-row justify-end'>
-            <CTSideBar />
+          <div className="w-full ">
+            <div className=' flex flex-row justify-end'>
+              <CTSideBar />
+            </div>
           </div>
         </div>
 
+      <div className='scale-50 -mt-26'>
+        <Head />
+      </div>
       </div>
 
-    <div className='scale-50 -mt-27'>
-      <Head />
-    </div>
-      </div>
-
-      <div className=" w-full  h-100 rounded-2xl flex flex-col items-center justify-center border-1 border-sea -mt-4">
-        {/* Scaled Button */}
-        <div className="-mt-57 flex flex-col justify-center items-center object-contain">
+      <div className=" w-full  h-130 rounded-2xl flex flex-col items-center justify-center border-1 border-sea -mt-4">
+        {/* main Job Card with details  */}
+        <div className=" flex flex-col justify-center items-center object-contain mb-2">
           <button className="bg-sea rounded-b-2xl p-2 px-5 font-bold scale-75 text-Ground mb-3 object-contain">
-            My Jobs
+            Job Card
           </button>
           
           {/* Job Card */}
           <div className="">
             {/* main Add Job Container */}
-            <div className="flex flex-row justify-between items-center  w-70 h-35 rounded-3xl border-2 bg-container -mt-3 ">
-              <div className=" w-35 h-30 rounded-3xl border-2 border-sea bg-NeonSea "></div>
-              <div className=" w-35 h-30 rounded-3xl border-2 border-sea bg-NeonSea "></div>
+            <div className="flex flex-col justify-evenly items-center  w-75 h-43 rounded-3xl border-2 bg-gold -mt-3 object-contain">
+              <div className='flex flex-row justify-between items-center gap-x-2 '>
+                <div className=" flex flex-row justify-evenly items-center gap-x-2 w-42 h-30 rounded-xl  bg-highGold drop-shadow-sm shadow shadow-2xl ">
+                  
+                 {/* Paper size */}
+                  <div className='flex flex-row justify-center items-center ml-1 w-18 h-26 rounded-md bg-ground border-1 drop-shadow-sm shadow shadow-md'>
+                    <div className='flex flex-col justify-evenly items-center'>
+                      {/* Size Selection */}
+                      <div className='text-center scale-130 mb-1 border-b-2  border-b-coHead text-coHead'>
+                        Size
+                      </div>
+                      <div className='font-extrabold text-4xl text-center text-gold '>
+                        A4
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Job Type and Quantity Container */}
+                  <div className='flex flex-col gap-3 mr-1 drop-shadow-sm shadow shadow-md'>
+                    <div className='w-18 h-8 rounded-md bg-ground text-center text-coHead flex flex-row justify-center items-center font-semibold drop-shadow-sm shadow shadow-md'> 
+                      Job Type
+                    </div>
+                    {/* Quantity button & display Container */}
+                    <div className=' flex flex-row justify-between items-center w-18 h-10 rounded-md bg-coHead'>
+                      <div className='flex flex-row -ml-1 items-center w-10 h-12 rounded-md scale-75  font-bold bg-ground '> 
+                        <div className='mr-0.5 ml-1 text-coHead'>QTY</div>
+                      </div>
+                      <div className='text-center mr-1 font-bold text-red-600'>00</div>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Color and Option section */}
+                <div className="flex- flex-col justify-center items-center w-30 h-25 rounded-2xl bg-highGold drop-shadow-sm shadow shadow-2xl">
+                  {/* Color, f/b and other selectors  */}
+                  <div className='flex flex-row gap-x-2 justify-center items-center scale-90  mb-2 mt-3'>
+                    <div className='w-18 h-8 rounded-md bg-high text-center flex flex-row justify-center items-center font-semibold'> 
+                      Color
+                    </div>
+                    <div className='w-18 h-8 rounded-md bg-inactive text-center flex flex-row justify-center items-center font-semibold'> 
+                     Black
+                    </div>
+                  </div>
+                  {/* Color and Black selectors  */}
+                  <div className='flex flex-row gap-x-2 justify-center items-center scale-90'>
+                    <div className='w-18 h-8 rounded-md bg-inactive font-bold text-center flex flex-row 
+                    justify-center items-center'> 
+                      F / B 
+                    </div>
+                    <div className='w-18 h-8 rounded-md bg-sea text-center flex flex-row justify-center items-center font-semibold'> 
+                     Add <span>+</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='flex flex-row justify-evenly gap-3 scale-90'>
+                <button className='bg-ground text-coHead px-4 py-1 rounded-md drop-shadow-sm shadow shadow-2xl'>check</button>
+                <button className='bg-neonSea text-coHead px-4 py-1 rounded-md drop-shadow-sm shadow shadow-2xl'>save order</button>
+              </div>
+
             </div>
           </div>
-
+        {/* dividing line */}
+        </div>
+        <div className='w-full border border-t-0.5 border-sea'></div>
+        <div className='w-full h-50 border-2 border-inactive'>
+          <button className="bg-sea rounded-b-2xl p-2 px-5 font-bold scale-75 text-Ground mb-3 object-contain">
+            Job Card
+          </button>
         </div>
 
       </div>
+
     </>
 
 
--- ellicom-hub/src/components/pages/CTJobList.jsx
+++ ellicom-hub/src/components/pages/CTJobList.jsx
@@ -8,78 +8,64 @@ import Head from '../UI/Head';
 const  CTJobList= () => {
   return (
     <>
-      <div className='border-gold border-b-1 h-23 mb-5'>
+      <div className='border-gold border-b-1 h-23 -mb-2'>
         <div className=" mt-10 flex flex-col items-center justify-between g  ">
-
-        <div className="w-full flex flex-row justify-between -ml-20 -mt-4">
-
-          <div className='scale-50'>
-            <NewJobButton />
-          </div>
-
-          <div className='-mr-10'>
-            <CTSideBar />
-          </div>
-        </div>
-
-      </div>
-
-    <div className='scale-50 -mt-27'>
-      <Head />
-    </div>
-      </div>
-
-          <div className=" w-full  h-100 rounded-2xl flex flex-col items-center justify-center border-1 border-sea mt-3">
-      {/* Scaled Button */}
-      <div className="-mt-50 flex flex-col justify-center items-center">
-        <button className="bg-sea rounded-b-2xl p-2 px-5 font-bold text-ground mb-4">
-          Current Jobs
-        </button>
-
-        <div className="">
-          <div className="flex flex-row justify-between mx-0.5 w-70 h-45 rounded-3xl border-2 bg-gold -mb-9 ">
-
-            <div className='ml-3'>
-              <div className='mb-1 font-bold text-center'>ID-EAG095</div>
-
-              <div className='w-45 h-35 bg-power rounded-2xl'></div>
+          <div className="w-full flex flex-row justify-between -ml-20 -mt-4">
+            <div className='scale-50'>
+              <NewJobButton />
             </div>
+            <div className='-mr-10'>
+              <CTSideBar />
+            </div>
+          </div>
+      </div>
 
+      <div className='scale-50 -mt-27'>
+        <Head />
+      </div>
+      </div>
 
-            <div className=''>
-
-              <div className=' flex flex-row justify-center items-center'>
-
-                <div className='flex w-15 h-20 bg-power rounded-b-2xl mb-2'>
-
-                  <div className='scale-75 '>
-                    <Logo />
-                    <p className='text-gold text-center '>
-                      Print
-                    </p>
+      <div className=" w-full  h-100 rounded-2xl flex flex-col items-center justify-center border-1 border-sea mt-3">
+        {/* Scaled Button */}
+        <div className="-mt-50 flex flex-col justify-center items-center">
+          <button className="bg-sea rounded-b-2xl p-2 px-5 font-bold text-ground mb-4">
+            Current Jobs
+          </button>
+          <div className="">
+            <div className="flex flex-row justify-between mx-0.5 w-70 h-45 rounded-3xl border-2 bg-gold -mb-9 ">
+              <div className='ml-3'>
+                <div className='mb-1 font-bold text-center'>ID-EAG095</div>
+                <div className='w-45 h-35 bg-power rounded-2xl'></div>
+              </div>
+              <div className=''>
+                <div className=' flex flex-row justify-center items-center'>
+                  <div className='flex w-15 h-20 bg-power rounded-b-2xl mb-2'>
+                    <div className='scale-75 '>
+                      <Logo />
+                      <p className='text-gold text-center '>
+                        Print
+                      </p>
+                    </div>
                   </div>
                 </div>
+
+                <div className='flex flex-col justify-center items-center gap-2'>
+
+                  <button className='bg-high p-1 rounded-sm px-3 font-medium'>
+                    Details
+                  </button>
+
+                  <button className='bg-sea p-1 rounded-sm px-3 font-medium'>
+                    Role
+                  </button>
+                </div>
+
               </div>
-
-              <div className='flex flex-col justify-center items-center gap-2'>
-
-                <button className='bg-high p-1 rounded-sm px-3 font-medium'>
-                  Details
-                </button>
-
-                <button className='bg-sea p-1 rounded-sm px-3 font-medium'>
-                  Role
-                </button>
-              </div>
-
-            </div>
 
           </div>
         </div>
 
       </div>
-
-      {/* Box Grid */}
     </div>
 
     </>
