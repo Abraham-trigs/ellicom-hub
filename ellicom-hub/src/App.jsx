1e9c2a73a7174a7c95c7a8954051cebe21d0ca5a
Abraham Bortey Danfa <abrahamtrigs@gmail.com>
2025-06-17 15:32:45 +0000

    App.js re structured for easy routing recognition

--- ellicom-hub/src/App.jsx
+++ ellicom-hub/src/App.jsx
@@ -1,32 +1,41 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+
+// Layout
 import LayoutWithNav from './components/UI/LayoutWithNavBar';
+
+// Universal Pages
 import WelcomePage from './components/pages/WelcomePage';
+import AddJobPage from './components/pages/AddJobPage';
+import JobDetailsPage from './components/pages/JobDetailsPage';
+
+// Client Pages
 import CTLPage from './components/pages/ClientPages/CTLPage';
+import CTJobCard from './components/UI/CLIENT-UI/CTJobCard';
+import CTJobList from './components/pages/ClientPages/CTJobList';
+
+// Staff Pages
 import SLPage from './components/pages/StaffPages/SLPage';
 import Home from './components/pages/StaffPages/Home';
 import SDashboard from './components/pages/StaffPages/SDashboard';
-import CTJobCard from './components/UI/CLIENT-UI/CTJobCard';
-import AddJobPage from './components/pages/AddJobPage';
-import JobDetailsPage from './components/pages/JobDetailsPage';
-import CTJobList from './components/pages/ClientPages/CTJobList';
 
 function App() {
   return (
+    
     <LayoutWithNav>
       <Routes>
 
         {/* Universal Welcome Page */}
         <Route path="/" element={<WelcomePage />} />
 
-        {/* Guest */}
+        {/* Guest Route */}
         <Route path="/guest/add-job" element={<AddJobPage />} />
 
-        {/* Staff */}
+        {/* Staff Routes */}
         <Route path="/staff/login" element={<SLPage />} />
         <Route path="/staff/home" element={<Home />} />
         <Route path="/staff/dashboard" element={<SDashboard />} />
 
-        {/* Client */}
+        {/* Client Routes */}
         <Route path="/client/login" element={<CTLPage />} />
         <Route path="/client/job-card" element={<CTJobCard />} />
         <Route path="/client/dashboard" element={<CTJobList />} />
